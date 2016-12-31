00262c0012d549a73df77c29ce14b3268e4c2a71
667 <667@june07.com>
2016-12-30 23:58:59 -0800

    no message

--- nim/notifications/4887dff5-7a99-4eae-bf9b-ad8c07e6fdc6.js
+++ nim/notifications/4887dff5-7a99-4eae-bf9b-ad8c07e6fdc6.js
@@ -1,25 +1,27 @@
-const notifications =
-[ {
-    'id': 0,
-    'notification': {
-        'title': 'Test Title',
-        'message': 'Test Message',
-        'icon': '',
-        'type': '',
-        'progress': '',
-        'url': '',
-        'target': ''
-    }
-}, {
-    'id': 1,
-    'notification': {
-        'title': 'New Logo',
-        'message': 'Would love to get your feedback on the new logo!',
-        'icon': '',
-        'type': '',
-        'progress': '',
-        'url': '',
-        'target': ''
-    }
-} ];
-return notifications;
+function() {
+    const notifications =
+    [ {
+        'id': 0,
+        'notification': {
+            'title': 'Test Title',
+            'message': 'Test Message',
+            'icon': '',
+            'type': '',
+            'progress': '',
+            'url': '',
+            'target': ''
+        }
+    }, {
+        'id': 1,
+        'notification': {
+            'title': 'New Logo',
+            'message': 'Would love to get your feedback on the new logo!',
+            'icon': '',
+            'type': '',
+            'progress': '',
+            'url': '',
+            'target': ''
+        }
+    } ];
+    return notifications;
+}
